e8a5ddbc0209694e709712a8b37e03b0565e061a
Your Name <you@example.com>
2026-08-02 19:53:02 +0530

    Remove duplicate Python files from repository root

--- 04user_input.py
+++
@@ -1,9 +0,0 @@
-# This program demonstrates Python's basic data types.
-# The type() function is used to identify each data type.
-
-
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-
-print("Hello", name)
-print("You are", age, "years old.")
--- 07for_loop.py
+++
@@ -1,5 +0,0 @@
-# This program demonstrates the use of a for loop.
-# A for loop executes a block of code for each item in a sequence.
-
-for i in range(1, 11):
-    print(i)
